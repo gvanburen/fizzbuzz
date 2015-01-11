7fc607f3649371c03cfe805f4d4dd8a04ee17086
Greg Van Buren <g.vanburen@gmail.com>
2015-01-11 13:33:51 -0800

    Added user input

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
-	$('.letsGo').mousedown(fizzBuzzy);
+	// $('.letsGo').mousedown(fizzBuzzy);
+	getMax();
 
-	function fizzBuzzy() {
+	function getMax() {
+	var maxNumber = prompt("Enter a maxiumum number");
+	maxNumber = +maxNumber;
+		
+		if (maxNumber % 1 != 0 || maxNumber == NaN) {
+			alert("Please enter a whole number");
+			getMax();
+		} else {
+			fizzBuzzy(maxNumber);
+		}
+	};
+
+	function fizzBuzzy(max) {
 		var fizz = "Fizz";
 		var buzz = "Buzz";
 		var fizzBuzz = "FizzBuzz"
 		var results = $('#fbResults');
 
-		for (theNumber = 1; theNumber <= 100; theNumber++) {
+		for (theNumber = 1; theNumber <= max; theNumber++) {
 			if (theNumber % 3 == 0 && theNumber % 5 == 0) {
-				results.append(fizzBuzz + "<br>");
+				results.append(fizzBuzz + "<br/>");
 			} else if (theNumber % 3 == 0) {
 				results.append(fizz + "<br/>");
 			} else if (theNumber % 5 == 0) {
